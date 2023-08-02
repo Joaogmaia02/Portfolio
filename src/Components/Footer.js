0e3e33b55be827bf8168ab6428f376b949a07bf7
Murilo Storari <adrian.murilo83@gmail.com>
2023-08-01 21:45:45 -0300

    Arrumado bug de imagem errada

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
 
                 <div className="flex float-right space-x-6">
                     <a href="https://www.linkedin.com/in/murilostorari/" target="_blank" rel="noopener noreferrer" data-te-toggle="tooltip" title="Meu LinkedIn"><img className="w-6 hover:scale-105 transition-all duration-200 cursor-pointer" src="./img/icons/linkedin.png" alt="Meu LinkedIn"></img></a>
-                    <a href="https://github.com/murilostorari" target="_blank" rel="noopener noreferrer" data-te-toggle="tooltip" title="Meu GitHub"><img className="w-6 hover:scale-105 transition-all duration-200 cursor-pointer" src="./img/icons/rocket.png" alt="Meu GitHub"></img></a>
+                    <a href="https://github.com/murilostorari" target="_blank" rel="noopener noreferrer" data-te-toggle="tooltip" title="Meu GitHub"><img className="w-6 hover:scale-105 transition-all duration-200 cursor-pointer" src="./img/icons/github.png" alt="Meu GitHub"></img></a>
                 </div>
             </div>
         </footer>
