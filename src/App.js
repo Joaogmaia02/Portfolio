1a72047f872380b6a0d4b6f4498018e08c6fa313
Murilo Storari <adrian.murilo83@gmail.com>
2023-08-02 15:27:34 -0300

    Alterado nome da pasta

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Skills from "./Components/Skills";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 export default function App() {
     return (
